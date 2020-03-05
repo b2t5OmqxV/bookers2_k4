aca26c8a19b37a4913bcabda38f0a63790475d1b
b2t5OmqxV <tatuyaizumi7@gmail.com>
2020-03-05 20:22:38 +0900

    [Finish]

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,:validatable
+
+  attachment :profile_image, destroy: false
+
+  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+  validates :name,  presence: true, length: {maximum: 20, minimum: 2}
+  validates :introduction, length: { maximum: 50 }
+
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+  
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
+
+end
